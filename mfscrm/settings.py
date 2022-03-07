eb8f0598f4d49f0d8eaa203d37b42f9d2fa0436e
dthorenUNO <dthoren@unomaha.edu>
2022-03-06 21:24:34 -0600

    updated settings

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w&5@!9)tqtgmxw^lt#6$@cz4+=$uhl4&4j$-+=skuicxv&&6wm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
